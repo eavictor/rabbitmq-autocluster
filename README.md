81df8ed45de07e499a77a38a8260715d42a8dc44
eavictor <eavictor@me.com>
2017-06-11 21:21:46 +0800

    add docker-compose for stack deploy (not tested)

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ http://www.kuznero.com/posts/docker/rabbitmq-dynamic-cluster.html
 
 ### How to build  image ??
 ```docker build -t <yourname>/rabbitmq-autocluster:latest .```
+
+### how to deploy ??
+
+see init_cluster.sh for ugly script.
+
+docker-compose.yml for docker stack deploy is not tested.
+
+```docker stack deploy --compose-file docker-compose.yml```
+
+### If you found RabbitMQ cluster is splitted
+
+1. ```docker service scale rabbit=0```
+2. increase the number slowly, +1 everytime
---
+++ init_cluster.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# create overlay network for RabbitMQ cluster
+sudo docker network create rabbit-net -d overlay
+
+# create consul discovery service
+sudo docker service create \
+--name consul \
+--network rabbit-net \
+-p 8500:8500 \
+-e 'CONSUL_BIND_INTERFACE=eth0' \
+-e 'CONSUL_LOCAL_CONFIG={"skip_leave_on_interrupt": true}' \
+consul agent \
+-server -ui -client=0.0.0.0 \
+-bootstrap-expect=1 \
+-retry-join=consul
+
+# create RabbitMQ service
+sudo docker service create \
+--name rabbit \
+--network rabbit-net \
+-p 5672:5672 \
+-p 15672:15672 \
+-e "AUTOCLUSTER_TYPE=consul" \
+-e "CONSUL_HOST=consul" \
+-e "CONSUL_PORT=8500" \
+-e "CONSUL_SVC=rabbitmq" \
+-e "CONSUL_SVC_ADDR_AUTO=true" \
+-e "AUTOCLUSTER_CLEANUP=true" \
+-e "CLEANUP_WARN_ONLY=false" \
+-e "RABBITMQ_ERLANG_COOKIE=secrect" \
+-e "RABBITMQ_DEFAULT_USER=eavictor" \
+-e "RABBITMQ_DEFAULT_PASS=mypasswd" \
+eavictor/rabbitmq-autocluster
+
+# scale the instance
+sudo docker service scale rabbit=3  # change the number if you want more instances
