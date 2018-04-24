e3f475c0e63c9c035575e43d8f286b621a167ca5
eavictor <eavictor@me.com>
2018-04-24 14:30:16 +0800

    Increase sleep time to 10 seconds.

--- wait-for-consul-and-start.sh
+++ wait-for-consul-and-start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Modify consul host if you plain to use other name
 CONSUL_HOST=consul
+
 
 # 1. Check consul service
 until curl get http://$CONSUL_HOST:8500/v1/agent/services ; do
-    >&2 echo "Consul service not available"
-    sleep 1
+    >&2 echo "[$date+'%Y/%m/%d %T'] Consul - Service for RabbitMQ auto cluster is unavailable, sleep 10 seconds."
+    sleep 10
 done
->&2 echo "Consul service is up !!"
+>&2 echo "[$date+'%Y/%m/%d %T'] Consul - Service for RabbitMQ auto cluster is available, start RabbitMQ Server."
+
 
 # 2. Start RabbitMQ service
 rabbitmq-server
