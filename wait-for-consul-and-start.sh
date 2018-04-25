4dae8559b8da1faac3700b4bed1cc1f6dc0514b2
eavictor <eavictor@me.com>
2018-04-25 15:13:38 +0800

    Fix shell script datetime output and improve readability

--- wait-for-consul-and-start.sh
+++ wait-for-consul-and-start.sh
@@ -6,11 +6,11 @@ CONSUL_HOST=consul
 
 
 # 1. Check consul service
-until curl get http://$CONSUL_HOST:8500/v1/agent/services ; do
-    >&2 echo "[$date+'%Y/%m/%d %T'] Consul - Service for RabbitMQ auto cluster is unavailable, sleep 10 seconds."
+until curl get http://$(CONSUL_HOST):8500/v1/agent/services ; do
+    >&2 echo "[$(date +'%Y/%m/%d %T')] Consul - Service for RabbitMQ auto cluster is unavailable, sleep 10 seconds."
     sleep 10
 done
->&2 echo "[$date+'%Y/%m/%d %T'] Consul - Service for RabbitMQ auto cluster is available, start RabbitMQ Server."
+>&2 echo "[$(date +'%Y/%m/%d %T')] Consul - Service for RabbitMQ auto cluster is available, start RabbitMQ Server."
 
 
 # 2. Start RabbitMQ service
