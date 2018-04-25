877fdf66dfd805fc9b7b00e39840ba2870c277b2
eavictor <eavictor@me.com>
2018-04-25 16:52:57 +0800

    Fix shell script variable syntax error $(xxx) --> ${xxx}

--- wait-for-consul-and-start.sh
+++ wait-for-consul-and-start.sh
@@ -6,7 +6,7 @@ CONSUL_HOST=consul
 
 
 # 1. Check consul service
-until curl get http://$(CONSUL_HOST):8500/v1/agent/services ; do
+until curl get http://${CONSUL_HOST}:8500/v1/agent/services ; do
     >&2 echo "[$(date +'%Y/%m/%d %T')] Consul - Service for RabbitMQ auto cluster is unavailable, sleep 10 seconds."
     sleep 10
 done
